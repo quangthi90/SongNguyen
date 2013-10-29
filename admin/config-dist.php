84852c6f77ad7641f624e653c5ecd1bcc557b868
bommer <bommer@bommerdesign.com>
2013-10-29 12:24:09 +0700

    - Create project
    Signed-off-by: bommer <bommer@bommerdesign.com>

---
+++ admin/config-dist.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/project/sn-edu/admin/');
+define('HTTP_CATALOG', 'http://localhost/project/sn-edu/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/project/sn-edu/admin/');
+define('HTTPS_CATALOG', 'http://localhost/project/sn-edu/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\Program Files\xampp\htdocs\Project\sn-edu/admin/');
+define('DIR_SYSTEM', 'F:\Program Files\xampp\htdocs\Project\sn-edu/system/');
+define('DIR_DATABASE', 'F:\Program Files\xampp\htdocs\Project\sn-edu/system/database/');
+define('DIR_LANGUAGE', 'F:\Program Files\xampp\htdocs\Project\sn-edu/admin/language/');
+define('DIR_TEMPLATE', 'F:\Program Files\xampp\htdocs\Project\sn-edu/admin/view/template/');
+define('DIR_CONFIG', 'F:\Program Files\xampp\htdocs\Project\sn-edu/system/config/');
+define('DIR_IMAGE', 'F:\Program Files\xampp\htdocs\Project\sn-edu/image/');
+define('DIR_CACHE', 'F:\Program Files\xampp\htdocs\Project\sn-edu/system/cache/');
+define('DIR_DOWNLOAD', 'F:\Program Files\xampp\htdocs\Project\sn-edu/download/');
+define('DIR_LOGS', 'F:\Program Files\xampp\htdocs\Project\sn-edu/system/logs/');
+define('DIR_CATALOG', 'F:\Program Files\xampp\htdocs\Project\sn-edu/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sn-edu');
+define('DB_PREFIX', '');
+?>
---
+++ config-dist.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/project/sn-edu/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/project/sn-edu/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\Program Files\xampp\htdocs\Project\sn-edu/catalog/');
+define('DIR_SYSTEM', 'F:\Program Files\xampp\htdocs\Project\sn-edu/system/');
+define('DIR_DATABASE', 'F:\Program Files\xampp\htdocs\Project\sn-edu/system/database/');
+define('DIR_LANGUAGE', 'F:\Program Files\xampp\htdocs\Project\sn-edu/catalog/language/');
+define('DIR_TEMPLATE', 'F:\Program Files\xampp\htdocs\Project\sn-edu/catalog/view/theme/');
+define('DIR_CONFIG', 'F:\Program Files\xampp\htdocs\Project\sn-edu/system/config/');
+define('DIR_IMAGE', 'F:\Program Files\xampp\htdocs\Project\sn-edu/image/');
+define('DIR_CACHE', 'F:\Program Files\xampp\htdocs\Project\sn-edu/system/cache/');
+define('DIR_DOWNLOAD', 'F:\Program Files\xampp\htdocs\Project\sn-edu/download/');
+define('DIR_LOGS', 'F:\Program Files\xampp\htdocs\Project\sn-edu/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sn-edu');
+define('DB_PREFIX', '');
+?>
