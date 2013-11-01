03597841ae77e56fb608e610a85f0f6be184b097
wmthiet90 <nvthiet.khtn08@gmail.com>
2013-11-01 22:32:31 +0700

    init template

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -10,8 +10,7 @@ class ControllerCommonHeader extends Controller {
 		}
         
         if (isset($this->session->data['error']) && !empty($this->session->data['error'])) {
-            $this->data['error'] = $this->session->data['error'];
-            
+            $this->data['error'] = $this->session->data['error'];            
             unset($this->session->data['error']);
         } else {
             $this->data['error'] = '';
@@ -40,105 +39,25 @@ class ControllerCommonHeader extends Controller {
 			$this->data['logo'] = '';
 		}		
 		
-		$this->language->load('common/header');
-		
-		$this->data['text_home'] = $this->language->get('text_home');
-		$this->data['text_wishlist'] = sprintf($this->language->get('text_wishlist'), (isset($this->session->data['wishlist']) ? count($this->session->data['wishlist']) : 0));
-		$this->data['text_shopping_cart'] = $this->language->get('text_shopping_cart');
-    	$this->data['text_search'] = $this->language->get('text_search');
-		$this->data['text_welcome'] = sprintf($this->language->get('text_welcome'), $this->url->link('account/login', '', 'SSL'), $this->url->link('account/register', '', 'SSL'));
-		$this->data['text_logged'] = sprintf($this->language->get('text_logged'), $this->url->link('account/account', '', 'SSL'), $this->customer->getFirstName(), $this->url->link('account/logout', '', 'SSL'));
-		$this->data['text_account'] = $this->language->get('text_account');
-    	$this->data['text_checkout'] = $this->language->get('text_checkout');
-				
+		$this->language->load('common/header');		
+		$this->data['text_home'] = $this->language->get('text_home');				
 		$this->data['home'] = $this->url->link('common/home');
-		$this->data['wishlist'] = $this->url->link('account/wishlist', '', 'SSL');
-		$this->data['logged'] = $this->customer->isLogged();
-		$this->data['account'] = $this->url->link('account/account', '', 'SSL');
-		$this->data['shopping_cart'] = $this->url->link('checkout/cart');
-		$this->data['checkout'] = $this->url->link('checkout/checkout', '', 'SSL');
 		
 		// Daniel's robot detector
-		$status = true;
-		
+		$status = true;		
 		if (isset($this->request->server['HTTP_USER_AGENT'])) {
 			$robots = explode("\n", trim($this->config->get('config_robots')));
 
 			foreach ($robots as $robot) {
 				if ($robot && strpos($this->request->server['HTTP_USER_AGENT'], trim($robot)) !== false) {
 					$status = false;
-
 					break;
 				}
 			}
 		}
 		
-		// A dirty hack to try to set a cookie for the multi-store feature
-		$this->load->model('setting/store');
-		
-		$this->data['stores'] = array();
-		
-		if ($this->config->get('config_shared') && $status) {
-			$this->data['stores'][] = $server . 'catalog/view/javascript/crossdomain.php?session_id=' . $this->session->getId();
-			
-			$stores = $this->model_setting_store->getStores();
-					
-			foreach ($stores as $store) {
-				$this->data['stores'][] = $store['url'] . 'catalog/view/javascript/crossdomain.php?session_id=' . $this->session->getId();
-			}
-		}
-				
-		// Search		
-		if (isset($this->request->get['search'])) {
-			$this->data['search'] = $this->request->get['search'];
-		} else {
-			$this->data['search'] = '';
-		}
-		
-		// Menu
-		$this->load->model('catalog/category');
-		
-		$this->load->model('catalog/product');
-		
-		$this->data['categories'] = array();
-					
-		$categories = $this->model_catalog_category->getCategories(0);
-		
-		foreach ($categories as $category) {
-			if ($category['top']) {
-				// Level 2
-				$children_data = array();
-				
-				$children = $this->model_catalog_category->getCategories($category['category_id']);
-				
-				foreach ($children as $child) {
-					$data = array(
-						'filter_category_id'  => $child['category_id'],
-						'filter_sub_category' => true
-					);
-					
-					$product_total = $this->model_catalog_product->getTotalProducts($data);
-									
-					$children_data[] = array(
-						'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $product_total . ')' : ''),
-						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
-					);						
-				}
-				
-				// Level 1
-				$this->data['categories'][] = array(
-					'name'     => $category['name'],
-					'children' => $children_data,
-					'column'   => $category['column'] ? $category['column'] : 1,
-					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
-				);
-			}
-		}
-		
 		$this->children = array(
-			'module/language',
-			'module/currency',
-			'module/cart'
+			'module/language'
 		);
 				
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/header.tpl')) {
--- catalog/view/theme/default/template/common/home.tpl
+++ catalog/view/theme/default/template/common/home.tpl
@@ -1,5 +1,32 @@
-<?php echo $header; ?><?php echo $column_left; ?><?php echo $column_right; ?>
-<div id="content"><?php echo $content_top; ?>
-<h1 style="display: none;"><?php echo $heading_title; ?></h1>
-<?php echo $content_bottom; ?></div>
+<?php echo $header; ?>
+<div id="body-content">
+	<div class="pagelocation">
+		<a href="duhoche.html">DU HỌC HÈ</a>
+	</div>
+	<div class="box5">
+		<a class="example7 cboxElement" href="duhochesing.html">
+			<img src="<?php echo $categoryImgUrl; ?>/banner-duhocsing.jpg" width="300" height="204" alt="Du Học Hè Singapore">
+		</a>
+	</div>
+	<div class="box5">
+		<a class="example7 cboxElement" href="duhochemy.html">
+			<img src="<?php echo $categoryImgUrl; ?>/banner-duhocmy.jpg" width="300" height="204" alt="Du Học Mỹ">
+		</a>
+	</div>
+	<div class="box5">
+		<a class="example7 cboxElement" href="duhocheuc.html">
+			<img src="<?php echo $categoryImgUrl; ?>/banner-duhocuc.jpg" width="300" height="204" alt="Du Học Úc">
+		</a>
+	</div>
+	<div class="box5">
+		<a class="example7 cboxElement" href="duhochenewzealand.html">
+			<img src="<?php echo $categoryImgUrl; ?>/banner-duhocnewzealand.jpg" width="300" height="204" alt="Du Học New Zealand">
+		</a>
+	</div>
+	<div class="box5">
+		<a class="example7 cboxElement" href="duhocheanh.html">
+			<img src="<?php echo $categoryImgUrl; ?>/banner-duhocanh.jpg" width="300" height="204" alt="Du Học Anh">
+		</a>
+	</div>
+</div>
 <?php echo $footer; ?>
