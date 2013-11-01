814c88da567fe353604bd92f7e9272ff94ac501c
vn3352 <vn3352@bommerdesign.com>
2013-11-01 10:44:12 +0700

    - Update for list news, insert news.
    Signed-off-by: vn3352 <vn3352@bommerdesign.com>

---
+++ admin/language/english/news/news.php
@@ -0,0 +1,30 @@
+<?php
+// heading title
+$_['heading_title'] 		= 'News';
+
+// text
+$_['text_success']			= '';
+$_['text_browse']			= 'Browse';
+$_['text_clear']			= 'Clear';
+$_['text_image_manager']			= 'Image Manager';
+
+// column
+$_['column_title'] 			= 'Title';
+$_['column_image'] 			= 'Image';
+$_['column_status'] 			= 'Status';
+$_['column_action'] 			= 'Action';
+
+// Entry
+$_['entry_title'] 			= 'Title:';
+$_['entry_content'] 			= 'Content:';
+$_['entry_primary_image'] 			= 'Primary Image:';
+$_['entry_second_image'] 			= 'Second Image:';
+$_['entry_status'] 			= 'Status:';
+$_['entry_sort_order'] 			= 'Sort Order:';
+$_['entry_news_category'] 			= 'News Category:';
+
+// error
+$_['error_permission']			= '';
+$_['error_title']			= '';
+$_['error_warning']			= '';
+?>
