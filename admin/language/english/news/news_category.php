78e624ba717fe4c913a98d5e6bd8e46923aac557
vn3352 <vn3352@bommerdesign.com>
2013-11-01 07:54:14 +0700

    - Update for list news category.
    Signed-off-by: vn3352 <vn3352@bommerdesign.com>

---
+++ admin/language/english/news/news_category.php
@@ -0,0 +1,10 @@
+<?php
+$_['heading_title']			= 'News Category';
+
+// text
+
+// column
+$_['column_name']            = 'News Category Name';
+$_['column_sort_order']      = 'Sort Order';
+$_['column_action']          = 'Action';
+?>
