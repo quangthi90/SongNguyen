c297d42672111bd20ca27c3e8d77d5cc82f1a708
vn3352 <vn3352@bommerdesign.com>
2013-11-04 08:37:06 +0700

    - Update for program management.
    Signed-off-by: vn3352 <vn3352@bommerdesign.com>

---
+++ admin/language/english/program/program.php
@@ -0,0 +1,29 @@
+<?php
+// heading title
+$_['heading_title'] 		= 'Program';
+
+// text
+$_['text_success']			= 'Success: You have modified program!';
+$_['text_browse']			= 'Browse';
+$_['text_clear']			= 'Clear';
+$_['text_image_manager']			= 'Image Manager';
+
+// column
+$_['column_title'] 			= 'Title';
+$_['column_image'] 			= 'Image';
+$_['column_status'] 			= 'Status';
+$_['column_action'] 			= 'Action';
+
+// Entry
+$_['entry_title'] 			= 'Title:';
+$_['entry_content'] 			= 'Content:';
+$_['entry_keyword'] 			= 'Keyword:';
+$_['entry_image'] 			= 'Image:';
+$_['entry_status'] 			= 'Status:';
+$_['entry_sort_order'] 			= 'Sort Order:';
+
+// error
+$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']       = 'Warning: You do not have permission to modify program!';
+$_['error_title']            = 'Program Title must be between 2 and 255 characters!';
+?>
