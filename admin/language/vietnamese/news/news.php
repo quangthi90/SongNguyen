9248a614b45f99c21ea71e110e2f3b9b4b994762
vn3352 <vn3352@bommerdesign.com>
2013-11-05 19:41:54 +0700

    - Update for new & news category language

---
+++ admin/language/vietnamese/news/news.php
@@ -0,0 +1,37 @@
+<?php
+// heading title
+$_['heading_title'] 		= 'Tin Tức';
+
+// text
+$_['text_success']			= 'Thành công: Bạn đã sửa đổi tin tức!';
+$_['text_browse']			= 'Duyệt';
+$_['text_clear']			= 'Dọn Dẹp';
+$_['text_image_manager']			= 'Quản Lý Hình Ảnh';
+$_['text_format_one']			= 'Định Dạng 1';
+$_['text_format_two']			= 'Định Dạng 2';
+
+// column
+$_['column_title'] 			= 'Tiêu Đề';
+$_['column_news_category_name'] 			= 'Danh Mục';
+$_['column_image'] 			= 'Hình Ảnh';
+$_['column_status'] 			= 'Tình Trạng';
+$_['column_action'] 			= 'Hoạt Động';
+
+// button
+$_['button_add_option'] 			= 'Add Option';
+
+// Entry
+$_['entry_title'] 			= 'Tiêu Đề:';
+$_['entry_content'] 			= 'Nội Dung:';
+$_['entry_primary_image'] 			= 'Hình Ảnh Chính:';
+$_['entry_second_image'] 			= 'Hình Ảnh Phụ:';
+$_['entry_format'] 			= 'Định Dạng:';
+$_['entry_status'] 			= 'Tình Trạng:';
+$_['entry_sort_order'] 			= 'Thứ Tự Sắp Xếp:';
+$_['entry_news_category'] 			= 'Danh Mục Tin Tức:';
+
+// error
+$_['error_warning']          = 'Cảnh báo: Vui lòng kiểm tra biểu mẫu cẩn thận cho các lỗi!';
+$_['error_permission']       = 'Cảnh báo: Bạn không có quyền sửa đổi Tin Tức!';
+$_['error_title']            = 'Tiêu Đề Tin Tức phải có từ 2 đến 255 ký tự!';
+?>
