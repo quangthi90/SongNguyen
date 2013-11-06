e06b55c79f91db9a042090dd92356804ac834a70
vn3352 <vn3352@bommerdesign.com>
2013-11-06 20:34:39 +0700

    - Update for popup & program language.
    Signed-off-by: vn3352 <vn3352@bommerdesign.com>

---
+++ admin/language/vietnamese/program/program.php
@@ -0,0 +1,29 @@
+<?php
+// heading title
+$_['heading_title'] 		= 'Chương trình';
+
+// text
+$_['text_success']			= 'Thành công: Bạn đã sửa đổi chương trình!';
+$_['text_browse']			= 'Duyệt';
+$_['text_clear']			= 'Dọn Dẹp';
+$_['text_image_manager']			= 'Quản Lý Hình Ảnh';
+
+// column
+$_['column_title'] 			= 'Tiêu Đề';
+$_['column_image'] 			= 'Hình Ảnh';
+$_['column_status'] 			= 'Tình Trạng';
+$_['column_action'] 			= 'Hoạt Động';
+
+// Entry
+$_['entry_title'] 			= 'Tiêu Đề:';
+$_['entry_content'] 			= 'Nội Dung:';
+$_['entry_keyword'] 			= 'Từ Khóa:';
+$_['entry_image'] 			= 'Hình Ảnh:';
+$_['entry_status'] 			= 'Tình Trạng:';
+$_['entry_sort_order'] 			= 'Thứ Tự Sắp Xếp:';
+
+// error
+$_['error_warning']          = 'Cảnh báo: Vui lòng kiểm tra biểu mẫu cẩn thận cho các lỗi!';
+$_['error_permission']       = 'Cảnh báo: Bạn không có quyền sửa đổi chương trình!';
+$_['error_title']            = 'Tiêu Đề chương trình phải có từ 2 đến 255 ký tự!';
+?>
