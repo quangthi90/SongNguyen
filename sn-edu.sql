08c765a79e5406d9b9743d39c0a815726b3e9c0d
bommer <bommer@bommerdesign.com>
2013-11-07 00:18:22 +0700

    - Update database
    Signed-off-by: bommer <bommer@bommerdesign.com>

--- sn-edu.sql
+++ sn-edu.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2013 at 06:12 PM
+-- Generation Time: Nov 06, 2013 at 06:18 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
