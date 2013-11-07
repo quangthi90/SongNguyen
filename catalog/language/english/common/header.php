f6b1416530dfc5d35ddf5a06f3654594d2cddcd6
vn3352 <vn3352@bommerdesign.com>
2013-11-07 18:50:13 +0700

    - Update for menu.

--- catalog/language/english/common/header.php
+++ catalog/language/english/common/header.php
@@ -8,4 +8,10 @@ $_['text_welcome']        = 'Welcome visitor you can <a href="%s">login</a> or <
 $_['text_logged']         = 'You are logged in as <a href="%s">%s</a> <b>(</b> <a href="%s">Logout</a> <b>)</b>';
 $_['text_account']        = 'My Account';
 $_['text_checkout']       = 'Checkout';
+$_['text_program'] = 'Programs';
+$_['text_event'] = 'News & Event';
+$_['text_faq'] = 'FAQs';
+$_['text_contact_inf'] = 'Contact Information';
+$_['text_send_email'] = 'Send Email to Song Nguyen';
+$_['text_support_onl'] = 'Support Online';
 ?>
