cf9ba036368d1d9f582834d17c015789658cb25b
vn3352 <vn3352@bommerdesign.com>
2013-11-08 21:56:09 +0700

    - Update for contact.
    Signed-off-by: vn3352 <vn3352@bommerdesign.com>

--- catalog/controller/contact/contact.php
+++ catalog/controller/contact/contact.php
@@ -63,6 +63,8 @@ class ControllerContactContact extends Controller {
 		$this->data['google_analytics'] = html_entity_decode($this->config->get('config_google_analytics'), ENT_QUOTES, 'UTF-8');
 	 
     	if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+			$this->load->model('contact/contact');	
+			$this->model_contact_contact->addContact($this->request->post);
 			/*$mail = new Mail();
 			$mail->protocol = $this->config->get('config_mail_protocol');
 			$mail->parameter = $this->config->get('config_mail_parameter');
