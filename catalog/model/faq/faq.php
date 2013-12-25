047ea79b461940564c540bf821cc3de67c1d351c
bommer <bommer@bommerdesign.com>
2013-12-25 16:01:34 +0700

    - Update sort faqs for front end
    Signed-off-by: bommer <bommer@bommerdesign.com>

--- catalog/model/faq/faq.php
+++ catalog/model/faq/faq.php
@@ -1,40 +1,5 @@
 <?php
 class ModelFaqFaq extends Model {
-/*	public function addFaq($data) {
-		$this->db->query("INSERT INTO " . DB_PREFIX . "faq SET status = '" . (int)$data['status'] . "', sort_order = '" . (int)$data['sort_order'] . "', faq_category_id = '" . (int)$data['faq_category_id'] . "', date_added = NOW(), date_modified = NOW()");
-		
-		$faq_id = $this->db->getLastId();
-		
-		foreach ($data['faq_description'] as $language_id => $value) {
-			$this->db->query("INSERT INTO " . DB_PREFIX . "faq_description SET faq_id = '" . (int)$faq_id . "', language_id = '" . (int)$language_id . "', question = '" . $this->db->escape($value['question']) . "', answer = '" . $this->db->escape($value['answer']) . "'");
-		}
-		
-		$this->cache->delete('faq');
-	}
-	
-	public function editFaq($faq_id, $data) {
-		$this->db->query("UPDATE " . DB_PREFIX . "faq SET status = '" . (int)$data['status'] . "', sort_order = '" . (int)$data['sort_order'] . "', faq_category_id = '" . (int)$data['faq_category_id'] . "', date_modified = NOW() WHERE id = '" . (int)$faq_id . "'");
-		
-		$this->db->query("DELETE FROM " . DB_PREFIX . "faq_description WHERE faq_id = '" . (int)$faq_id . "'");
-		
-		foreach ($data['faq_description'] as $language_id => $value) {
-			$this->db->query("INSERT INTO " . DB_PREFIX . "faq_description SET faq_id = '" . (int)$faq_id . "', language_id = '" . (int)$language_id . "', question = '" . $this->db->escape($value['question']) . "', answer = '" . $this->db->escape($value['answer']) . "'");
-		}
-	}
-	
-	public function deleteFaq($faq_id) {
-		$this->db->query("DELETE FROM " . DB_PREFIX . "faq WHERE id = '" . (int) $faq_id . "'");
-		$this->db->query("DELETE FROM " . DB_PREFIX . "faq_description WHERE faq_id = '" . (int) $faq_id . "'");
-		
-		$this->cache->delete('faq');
-	}
-	
-	public function getFaq($faq_id) {
-		$query = $this->db->query("SELECT DISTINCT f.id AS faq_id, f.sort_order AS sort_order, f.status AS status, f.faq_category_id AS faq_category_id FROM " . DB_PREFIX . "faq f LEFT JOIN " . DB_PREFIX . "faq_description fd ON (f.id = fd.faq_id) WHERE f.id = '" . (int)$faq_id . "' AND fd.language_id = '" . (int)$this->config->get('config_language_id') . "'");
-				
-		return $query->row;
-	}
-*/	
 	public function getFaqs($data = array()) {
 		$sql = "SELECT f.id AS faq_id, f.sort_order AS sort_order, f.status AS status, f.faq_category_id AS faq_category_id, fd.question AS question, fd.answer AS answer, fcd.name AS faq_category_name FROM " . DB_PREFIX . "faq f LEFT JOIN " . DB_PREFIX . "faq_description fd ON (f.id = fd.faq_id) LEFT JOIN " . DB_PREFIX . "faq_category fc ON (fc.id = f.faq_category_id) LEFT JOIN faq_category_description fcd ON (fc.id = fcd.faq_category_id)";
 				
@@ -68,13 +33,13 @@ class ModelFaqFaq extends Model {
 		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
 			$sql .= " ORDER BY " . $data['sort'];	
 		} else {
-			$sql .= " ORDER BY f.date_added";	
+			$sql .= " ORDER BY f.sort_order";	
 		}
 		
-		if (isset($data['order']) && ($data['order'] == 'ASC')) {
-			$sql .= " ASC";
-		} else {
+		if (isset($data['order']) && ($data['order'] == 'DESC')) {
 			$sql .= " DESC";
+		} else {
+			$sql .= " ASC";
 		}
 	
 		if (isset($data['start']) || isset($data['limit'])) {
