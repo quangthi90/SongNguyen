f5ca5b3c766411054a7db8161cb3f0143fefa8e1
bommer <bommer@bommerdesign.com>
2014-01-09 00:00:45 +0700

    - Add not size for popup carousel images
    Signed-off-by: bommer <bommer@bommerdesign.com>

--- admin/language/english/design/banner.php
+++ admin/language/english/design/banner.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']      = 'Banners';
+$_['heading_title']      = 'Banners (900x450)';
 
 // Text
 $_['text_success']       = 'Success: You have modified banners!';
